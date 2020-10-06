ac1444feec19b9439857a6947979c151f19d1910
qubiak <kubiak1986@gmail.com>
2020-10-06 10:42:26 +0200

    start commit

---
+++ src/main/java/pl/qubiak/covidmapbyqubiak/covidmapbyqubiakapplication.java
@@ -0,0 +1,13 @@
+package pl.qubiak.covidmapbyqubiak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidMapByQubiakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidMapByQubiakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/qubiak/covidmapbyqubiak/covidmapbyqubiakapplicationtests.java
@@ -0,0 +1,13 @@
+package pl.qubiak.covidmapbyqubiak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidMapByQubiakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
